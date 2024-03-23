c3f27376000ec530f5a370135be430b30b0de2f7
Ferozmiakhail <136775226+Ferozmiakhail@users.noreply.github.com>
2024-03-23 07:27:27 +0430

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { Navbar, Herobanner, Footerbanner } from "@/components/index";
 
 
 const Page = () => {
     return (
         <>
+            <Navbar />
+            <Herobanner />
+
 
             page
 
+
+            <Footerbanner />
         </>
         
     )
